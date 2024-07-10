bf8a180806711e7d7a8fe3f25fa5fd03c3f0f829
Monica Riegel <monicariegel@hotmail.com>
2024-07-09 22:30:42 -0300

    Criando banner e titulo

--- src/pages/Inicio/index.js
+++ src/pages/Inicio/index.js
@@ -1,10 +1,16 @@
+import Banner from "../../componentes/Banner"
 import Cabecalho from "../../componentes/Cabecalho"
 import Rodape from "../../componentes/Rodape"
+import Titulo from "../../componentes/Titulo"
 
 const Inicio = () => {
     return (
         <main>
             <Cabecalho />
+            <Banner imagem="home" />
+            <Titulo>
+                <h1>Um lugar para guardar seus videos e filmes!</h1>
+            </Titulo>
             <Rodape />
         </main>
     )
