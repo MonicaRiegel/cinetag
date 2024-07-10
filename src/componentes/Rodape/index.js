01690a15b3ad15e8cb88e549991f53c8cf3123be
Monica Riegel <monicariegel@hotmail.com>
2024-07-09 22:00:01 -0300

    Criando cabeçalho e rodapé

---
+++ src/componentes/Rodape/index.js
@@ -0,0 +1,11 @@
+import styles from './Rodape.module.css';
+
+const Rodape = () => {
+    return (
+        <footer className={styles.rodape}>
+            <h3>Desenvolvido pro Mônica Riegel durante treinamento da Alura</h3>
+        </footer>
+    )
+}
+
+export default Rodape
---
+++ src/pages/Inicio/index.js
@@ -0,0 +1,13 @@
+import Cabecalho from "../../componentes/Cabecalho"
+import Rodape from "../../componentes/Rodape"
+
+const Inicio = () => {
+    return (
+        <main>
+            <Cabecalho />
+            <Rodape />
+        </main>
+    )
+}
+
+export default Inicio
