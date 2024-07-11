8bb6f6a70089d231e2ff5f71eb29543b253ce203
Monica Riegel <monicariegel@hotmail.com>
2024-07-10 21:45:47 -0300

    Criando pagina favoritos

--- src/pages/Inicio/index.js
+++ src/pages/Inicio/index.js
@@ -1,18 +1,22 @@
-import Banner from "../../componentes/Banner"
-import Cabecalho from "../../componentes/Cabecalho"
-import Rodape from "../../componentes/Rodape"
-import Titulo from "../../componentes/Titulo"
+import Banner from "../../componentes/Banner";
+import Card from "../../componentes/Card";
+import Titulo from "../../componentes/Titulo";
+import videos from '../../json/db.json';
+import styles from './Inicio.module.css';
 
 const Inicio = () => {
     return (
-        <main>
-            <Cabecalho />
+        <>
             <Banner imagem="home" />
             <Titulo>
                 <h1>Um lugar para guardar seus videos e filmes!</h1>
             </Titulo>
-            <Rodape />
-        </main>
+            <section className={styles.container}>
+                {videos.map((video) => {
+                    return <Card {...video} key={video.id} />
+                })}
+            </section>
+        </>
     )
 }
 
--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,24 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom"
-import Inicio from "./pages/Inicio"
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Inicio from "./pages/Inicio";
+import Favoritos from "./pages/Favoritos";
+import Cabecalho from "./componentes/Cabecalho";
+import Rodape from "./componentes/Rodape";
+import Container from "./componentes/Container";
+import FavoritosProvider from "./contextos/Favoritos";
 
 const AppRoutes = () => {
     return (
         <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Inicio />}></Route>
-            </Routes>
+            <Cabecalho />
+            <Container>
+                <FavoritosProvider>
+                    <Routes>
+                        <Route path="/" element={<Inicio />}></Route>
+                        <Route path="/favoritos" element={<Favoritos />}></Route>
+                    </Routes>
+                </FavoritosProvider>
+            </Container>
+            <Rodape />
         </BrowserRouter>
     )
 }
